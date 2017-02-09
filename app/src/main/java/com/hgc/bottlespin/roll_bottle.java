bdd16b9be96baf173c980313ea582110e3b6c428
HARSHIT GUPTA <harshitsaamu@gmail.com>
2017-02-09 22:37:51 +0530

    initial

--- app/src/main/java/com/hgc/bottlespin/roll_bottle.java
+++ app/src/main/java/com/hgc/bottlespin/roll_bottle.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -16,6 +17,7 @@ public class roll_bottle extends AppCompatActivity {
     Random rand;
     int angle,changed_angle=0;
     int dialerHeight;
+    int player,total_player=4;
     private GestureDetector gestureListener;
     int dialerWidth;
     boolean isRotating=false;
@@ -37,18 +39,53 @@ public class roll_bottle extends AppCompatActivity {
 //                }
 //            }
 //        });
-        if (!isRotating)
+//        if (!isRotating)
         bottle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                angle=rand.nextInt(3600)+720;
+                player=rand.nextInt(total_player);
+                angle=player*360/total_player;
+//                angle=rand.nextInt(7200)+720;
                 RotateAnimation ranim=new RotateAnimation(changed_angle,angle,RotateAnimation.RELATIVE_TO_SELF,0.5f
                 ,RotateAnimation.RELATIVE_TO_SELF,0.5f);
-                changed_angle=angle%360;
-               ranim.setFillAfter(true);
-                ranim.setDuration(7200);
+                changed_angle=angle;
+                ranim.setFillAfter(true);
+                ranim.setDuration(3600);
                 ranim.setInterpolator(new AccelerateDecelerateInterpolator());
                 bottle.startAnimation(ranim);
+                switch (player)
+                {
+                    case 1:
+                        Toast.makeText(roll_bottle.this, "Player 1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(roll_bottle.this, "Player 2", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 3:
+                        Toast.makeText(roll_bottle.this, "Player 3", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 4:
+                        Toast.makeText(roll_bottle.this, "Player 4", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+//                if (changed_angle>=0 && changed_angle<90)
+//                {
+//                    Toast.makeText(roll_bottle.this, "Player 1", Toast.LENGTH_SHORT).show();
+//                }
+//                else if(changed_angle>=90 && changed_angle<180)
+//                {
+//                    Toast.makeText(roll_bottle.this, "Player 2", Toast.LENGTH_SHORT).show();
+//                }
+//                else if(changed_angle>=180 && changed_angle<270)
+//                {
+//                    Toast.makeText(roll_bottle.this, "Player 3", Toast.LENGTH_SHORT).show();
+//                }
+//                else
+//                {
+//                    Toast.makeText(roll_bottle.this, "Player 4", Toast.LENGTH_SHORT).show();
+//                }
             }
         });
     }
