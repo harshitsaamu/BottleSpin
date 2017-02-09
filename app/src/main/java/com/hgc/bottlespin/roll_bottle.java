5f7f351baf351c80101a64953758cf463b084dbc
HARSHIT GUPTA <harshitsaamu@gmail.com>
2017-02-09 23:48:49 +0530

    updated rotation

--- app/src/main/java/com/hgc/bottlespin/roll_bottle.java
+++ app/src/main/java/com/hgc/bottlespin/roll_bottle.java
@@ -2,7 +2,6 @@ package com.hgc.bottlespin;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.GestureDetector;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
@@ -17,12 +16,7 @@ public class roll_bottle extends AppCompatActivity {
     ImageView bottle,table;
     Random rand;
     int angle,changed_angle=0;
-    int dialerHeight;
     int player,total_player=10;
-    private GestureDetector gestureListener;
-    int dialerWidth;
-    boolean isRotating=false;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,16 +25,6 @@ public class roll_bottle extends AppCompatActivity {
         bottle=(ImageView)findViewById(R.id.id_bottle);
         table=(ImageView)findViewById(R.id.id_table);
         rand=new Random();
-//        bottle.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-//            @Override
-//            public void onGlobalLayout() {
-//                if( dialerHeight==0 || dialerWidth==0){
-//                    dialerHeight=bottle.getHeight();
-//                    dialerWidth=bottle.getWidth();
-//                }
-//            }
-//        });
-//        if (!isRotating)
         bottle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -104,23 +88,6 @@ public class roll_bottle extends AppCompatActivity {
 
                     }
                 });
-
-//                if (changed_angle>=0 && changed_angle<90)
-//                {
-//                    Toast.makeText(roll_bottle.this, "Player 1", Toast.LENGTH_SHORT).show();
-//                }
-//                else if(changed_angle>=90 && changed_angle<180)
-//                {
-//                    Toast.makeText(roll_bottle.this, "Player 2", Toast.LENGTH_SHORT).show();
-//                }
-//                else if(changed_angle>=180 && changed_angle<270)
-//                {
-//                    Toast.makeText(roll_bottle.this, "Player 3", Toast.LENGTH_SHORT).show();
-//                }
-//                else
-//                {
-//                    Toast.makeText(roll_bottle.this, "Player 4", Toast.LENGTH_SHORT).show();
-//                }
             }
         });
     }
