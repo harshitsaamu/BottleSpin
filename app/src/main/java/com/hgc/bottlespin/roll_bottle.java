ce97665798477a5aa8e3ff0b06975b5e05774062
HARSHIT GUPTA <harshitsaamu@gmail.com>
2017-02-11 01:37:48 +0530

    running car added

--- app/src/main/java/com/hgc/bottlespin/roll_bottle.java
+++ app/src/main/java/com/hgc/bottlespin/roll_bottle.java
@@ -1,11 +1,13 @@
 package com.hgc.bottlespin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -15,27 +17,37 @@ import java.util.Random;
 public class roll_bottle extends AppCompatActivity {
     ImageView bottle,table;
     Random rand;
+    Button button;
+    int flag=11;
     int angle,changed_angle=0;
     int player,total_player=10;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roll_bottle);
-
+        button=(Button)findViewById(R.id.butt);
         bottle=(ImageView)findViewById(R.id.id_bottle);
         table=(ImageView)findViewById(R.id.id_table);
         rand=new Random();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(roll_bottle.this,select_player.class);
+                startActivity(i);
+            }
+        });
         bottle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 player=rand.nextInt(total_player*100)/100;
+                flag=player;
                 angle=(player*(360/total_player))+7200;
 //                angle=rand.nextInt(7200)+720;
                 RotateAnimation ranim=new RotateAnimation(changed_angle,angle,RotateAnimation.RELATIVE_TO_SELF,0.5f
                 ,RotateAnimation.RELATIVE_TO_SELF,0.5f);
                 changed_angle=angle%360;
                 ranim.setFillAfter(true);
-                ranim.setDuration(3600);
+                ranim.setDuration(7200);
                 ranim.setInterpolator(new AccelerateDecelerateInterpolator());
                 bottle.startAnimation(ranim);
                 ranim.setAnimationListener(new Animation.AnimationListener() {
---
+++ app/src/main/java/com/hgc/bottlespin/select_player.java
@@ -0,0 +1,13 @@
+package com.hgc.bottlespin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class select_player extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_player);
+    }
+}
